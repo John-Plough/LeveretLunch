0d93a7f060b6d1b3a7edf25cf7ef995080bbee66
John Plough <jplough@gmail.com>
2023-12-14 13:12:36 -0500

    working eat func

---
+++ eat.js
@@ -0,0 +1,18 @@
+/**
+EAT - func eat
+  params: current cell (curCell)
+  add carrots in cell to total carrot count
+  update carrots in cell to 0
+
+Each cell is an obj with carrot count and indices:
+  { carrots: [row, col] }
+ */
+
+let carrotCount = 0;
+
+function eat(curCell) {
+  let cellCarrots = Object.keys(curCell)[0]; //carrots in cell
+  let location = curCell[cellCarrots]; //indices of cell
+  carrotCount += Number(cellCarrots); //add carrots to total
+  curCell = { 0: [location] }; //update cell carrots to 0
+}
