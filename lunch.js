c27235b4c380e8512516e6329a77bf117ea05b64
John Plough <jplough@gmail.com>
2023-12-14 12:19:46 -0500

    general overview notes

--- lunch.js
+++ lunch.js
@@ -1,11 +1,44 @@
 /** lunchCount: return # of carrots eaten
- *
- * params:
- * - garden: matrix of carrot counts
- *
- * returns: # of carrots
+ *input - garden (matrix of carrot counts)
+ *output - # of carrots
  */
 
-function lunchCount(garden) {
+/** CREATE HELPER FUNCTIONS:
 
-}
+START - func findStart
+determine start pt
+
+EAT - func eat
+add carrots in cell to total
+and reset that cell to 0 carrots
+
+MOVE - func move
+check all directions
+compare vals
+if tied for highest, choose in WNES order
+if no carrots in W,N,E or S
+sleep
+
+return total (# of carrots) */
+
+function lunchCount(garden) {}
+
+[
+  [1, 1, 1],
+  [0, 1, 1],
+  [9, 1, 9],
+];
+
+[
+  [9, 9, 9, 9],
+  [9, 3, 1, 0],
+  [9, 1, 4, 2],
+  [9, 9, 1, 0],
+];
+
+[
+  [2, 3, 1, 4, 2, 2, 3],
+  [2, 3, 0, 4, 0, 3, 0],
+  [1, 7, 0, 2, 1, 2, 3],
+  [9, 3, 0, 4, 2, 0, 3],
+];
